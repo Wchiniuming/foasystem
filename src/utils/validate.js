0a7de19b61d7b47eba7ab850beb70321d35c11ed
Wchiniuming <zmty0323@126.com>
2021-05-10 10:32:22 +0800

    编写前端导入数据时的格式验证validate，待完善

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -5,34 +5,34 @@ export const caseFormatValidate = (reader) => {
   const worker = xlsx.read(reader, { type: 'binary' })
   // 转换为json
   const readerSheets = xlsx.utils.sheet_to_json(worker.Sheets[worker.SheetNames[0]])
-  let ckCid = true
-  let ckDtime = true
+  let flag = true
   readerSheets.forEach(item => {
-    if (isNotNull(item['用例编号'])) {
-      console.log(item['用例编号'])
-      console.log('isNotNull', isNotNull(item['用例编号']))
-      ckCid = false
-    }
     if (item['测试日期'] !== '' && !isDate(item['测试日期'])) {
-      console.log('isDate', isDate(item['测试日期']))
-      ckDtime = false
+      console.log(item['用例编号'] + '的测试日期：isDate', isDate(item['测试日期']))
+      flag = false
+    }
+    if (isFormat(item['用例编号'])) {
+      console.log(item['用例编号'] + '：格式不正确', isDate(item['测试日期']))
+      flag = false
     }
   })
-  return ckCid && ckDtime
+  return flag
 }
 
+// 日期格式校验
 function isDate (str) {
-  var r = /^\d{4}-\d{1,2}-\d{1,2}$/
-  var lr = /^\d{4}-\d{1,2}-\d{1,2} \d{1,2}:\d{1,2}:\d{1,2}$/
-  if (r.test(str.trim()) || lr.test(str.trim())) {
+  const r = /^\d{4}-\d{1,2}-\d{1,2}$/
+  const lr = /^\d{4}-\d{1,2}-\d{1,2} \d{1,2}:\d{1,2}:\d{1,2}$/
+  if (!str || (str && (r.test(str.trim()) || lr.test(str.trim())))) {
     return true
   } else {
     return false
   }
 }
-
-function isNotNull (str) {
-  if (str && str !== '') {
+// TS00000用例编号格式校验
+function isFormat (str) {
+  const r = /^TS\d{5}$/
+  if (!str || (str && (r.test(str.trim())))) {
     return true
   } else {
     return false
